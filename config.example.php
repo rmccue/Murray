71693129c309558b3776e1b303067232ae2d0127
Ryan McCue <ryanmccue@cubegames.net>
2011-09-23 22:30:55 +1000

    Change example config

--- config.example.php
+++ config.example.php
@@ -2,14 +2,13 @@
 
 // Config
 
-Murray::$url = 'http://localhost/php/Murray';
-Murray::$db = new PDO('mysql:host=fserve;dbname=testdb', 'root', 'password');
+Murray::$url = 'http://localhost/';
+Murray::$db = new PDO('mysql:host=localhost;dbname=db', 'root', 'password');
 
 
 // Load sources
 
-Murray_Sources::load('Murray_Source_GitHub', 'rmccue');
-Murray_Sources::load('Murray_Source_Reddit', 'rmccue');
-Murray_Sources::load('Murray_Source_LastFM', 'rmccue');
-Murray_Sources::load('Murray_Source_Twitter', 'rmccue');
-//Murray_Sources::load('Murray_Source_Steam', array('user' => 'rmccue', 'key' => '4891AAE2F2D77FAC28D11EAC5766D0F2'));
+Murray_Sources::load('Murray_Source_GitHub', 'user');
+Murray_Sources::load('Murray_Source_Reddit', 'user');
+Murray_Sources::load('Murray_Source_LastFM', 'user');
+Murray_Sources::load('Murray_Source_Twitter', 'user');
